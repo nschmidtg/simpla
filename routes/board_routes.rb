cbbb9ab674a78f91783ae411fd93968b1c2d3be2
Nicolas <nschmidtg@gmail.com>
2016-06-20 16:48:30 -0400

    borrar bpmn

--- routes/board_routes.rb
+++ routes/board_routes.rb
@@ -8,12 +8,7 @@ require_relative '../utils/analyzers/board_details_analyzer'
 require_relative '../utils/fetchers/board_details_fetcher'
 
 class Ollert
-  get '/public/licitacion', :auth => :none do
-    
-
-    render file: Rails.public_path.join("licitacion","index.html"), layout: true # /public/templates/home.html
-
-  end
+ 
 
   get '/organizations', :auth => :connected do
     client = Trello::Client.new(
