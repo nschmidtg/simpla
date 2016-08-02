dc42f26f07af5c7b0660afd31a6236c32f5a8dcb
Nicolas Schmidt <nschmidtg@gmail.com>
2016-08-02 15:39:11 -0400

    secpla

--- routes/information_routes.rb
+++ routes/information_routes.rb
@@ -301,7 +301,7 @@ class Ollert
                     begin
                        
                         if(board.closed==false)
-                            puts user.member_token
+                            puts user.trello_id
                             board.closed=true
                             nombre=board.name
                             board.name="¡No puede crear proyectos de esa forma! Conctáctese con su Director Secpla. El tablero #{nombre} "
