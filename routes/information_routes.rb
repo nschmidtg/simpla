ffc08102e0db9f8c893aaf742d469bb30068d17c
Nicolas Schmidt <nschmidtg@gmail.com>
2016-08-02 12:51:03 -0400

    secpla

--- routes/information_routes.rb
+++ routes/information_routes.rb
@@ -294,7 +294,7 @@ class Ollert
         if(Board.find_by(board_id: board.id)==nil)
             begin
                 board.closed=true
-                board.name="¿No puede crear proyectos! Conctáctese con el Jefe Secpla"
+                board.name="¡No puede crear proyectos de esa forma! Conctáctese con el Director Secpla. Este tablero "
                 board.update!
                 puts "#{board.id} cerrado"
             rescue
