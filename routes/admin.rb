9c6e52fac4818f762a62b7211f6ba2f1f8b12db3
nicolas schmidt <nschmidtg@gmail.com>
2016-08-04 17:38:06 -0400

    admin

--- routes/admin.rb
+++ routes/admin.rb
@@ -114,8 +114,96 @@ class Ollert
       redirect '/admin'
       
     end
-  end
 
+  end
+post '/admin/create_municipio', :auth => :connected do
+    client = Trello::Client.new(
+      :developer_public_key => ENV['PUBLIC_KEY'],
+      :member_token => @user.member_token
+    )
+    if(@user.role!="admin")
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+      end
+    end
+    begin
+      
+      nombre=params[:name]
+      zonas=params[:zonas]
+      last_id=Municipio.first.id
+      id=last_id.to_i+1
+      mun=Municipio.new
+      mun.name=nombre
+      mun.save
+      zonas.each do |zone|
+
+        z=Zone.new
+        z.name=zone
+        z.municipio=mun
+        z.save
+        
+      end
+      estado1=State.new
+      estado1.name="No iniciado"
+      estado1.order="1"
+      estado1.municipio=mun
+      estado1.save
+
+      estado2=State.new
+      estado2.name="Formulación"
+      estado2.order="2"
+      estado2.municipio=mun
+      estado2.save
+
+      estado3=State.new
+      estado3.name="Observado"
+      estado3.order="3"
+      estado3.municipio=mun
+      estado3.save
+
+      estado4=State.new
+      estado4.name="Licitación"
+      estado4.order="4"
+      estado4.municipio=mun
+      estado4.save
+
+      estado5=State.new
+      estado5.name="Ejecución"
+      estado5.order="5"
+      estado5.municipio=mun
+      estado5.save
+      mun.save
+
+      puts mun.id
+      puts Zone.find_by(id: mun.zones.last.id).municipio.id
+    rescue Trello::Error => e
+      unless @user.nil?
+        @user.member_token = nil
+        @user.trello_name = nil
+        @user.save
+      end
+
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+
+        format.json { status 400 }
+      end
+    end
+
+    respond_to do |format|
+      flash[:succes] = "Municipio eliminado satisfactoriamente."
+      redirect '/admin'
+      
+    end
+
+    
+  end
 
 
 end
