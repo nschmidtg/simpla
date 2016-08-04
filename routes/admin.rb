0564180c5e967d4549702883bd76d80f1cc67fa9
nicolas schmidt <nschmidtg@gmail.com>
2016-08-04 15:42:12 -0400

    creacion de nuevo mun

--- routes/admin.rb
+++ routes/admin.rb
@@ -38,6 +38,44 @@ class Ollert
     end
   end
 
+  get '/admin/municipio', :auth => :connected do
+    client = Trello::Client.new(
+      :developer_public_key => ENV['PUBLIC_KEY'],
+      :member_token => @user.member_token
+    )
+    if(@user.role!="admin")
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+      end
+    end
+    begin
+      @municipio = Municipio.find_by(id: params[:id])
+    rescue Trello::Error => e
+      unless @user.nil?
+        @user.member_token = nil
+        @user.trello_name = nil
+        @user.save
+      end
+
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+
+        format.json { status 400 }
+      end
+    end
+
+    respond_to do |format|
+      format.html { haml :municipio }
+      
+    end
+  end
+
 
 
 end
