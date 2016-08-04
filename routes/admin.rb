21b8ab2d713f8bdf81d6fb05014e81a6344278f6
nicolas schmidt <nschmidtg@gmail.com>
2016-08-04 15:14:44 -0400

    asd

---
+++ routes/admin.rb
@@ -0,0 +1,43 @@
+require 'trello'
+class Ollert
+  get '/admin', :auth => :connected do
+    client = Trello::Client.new(
+      :developer_public_key => ENV['PUBLIC_KEY'],
+      :member_token => @user.member_token
+    )
+    if(@user.role!="admin")
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+      end
+    end
+    begin
+      @municipios = Municipio.all
+    rescue Trello::Error => e
+      unless @user.nil?
+        @user.member_token = nil
+        @user.trello_name = nil
+        @user.save
+      end
+
+      respond_to do |format|
+        format.html do
+          flash[:error] = "There's something wrong with the Trello connection. Please re-establish the connection."
+          redirect '/'
+        end
+
+        format.json { status 400 }
+      end
+    end
+
+    respond_to do |format|
+      format.html { haml :admin }
+      
+    end
+  end
+
+
+
+end
