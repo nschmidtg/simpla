45c2c077754c00aeef85b7dbc6e5bdf10135f3de
nicolas schmidt <nschmidtg@gmail.com>
2016-09-29 17:36:49 -0300

    diseño layout

--- public/js/ollert.js
+++ public/js/ollert.js
@@ -3,208 +3,208 @@ var Ollert = (function() {
   var teams = [];
   var info = [];
   var esAdmin=false;
-  var initDrawer = function() {
-    refreshDrawer();
-    $("body").toggleClass("has-drawer");
-    var hammertime = new Hammer($("body")[0]);
-    hammertime.on('swipeleft', function(ev) {
-      if ($("#config-drawer").hasClass("open")) {
-        $(".drawer-controls a").click();
-      }
-    });
-    hammertime.on('swiperight', function(ev) {
-      if (!$("#config-drawer").hasClass("open")) {
-        $(".drawer-controls a").click();
-      }
-    });
+  // var initDrawer = function() {
+  //   refreshDrawer();
+  //   $("body").toggleClass("has-drawer");
+  //   var hammertime = new Hammer($("body")[0]);
+  //   hammertime.on('swipeleft', function(ev) {
+  //     if ($("#config-drawer").hasClass("open")) {
+  //       $(".drawer-controls a").click();
+  //     }
+  //   });
+  //   hammertime.on('swiperight', function(ev) {
+  //     if (!$("#config-drawer").hasClass("open")) {
+  //       $(".drawer-controls a").click();
+  //     }
+  //   });
 
-    tempNotifyUsersAboutDrawer();
-  };
+  //   tempNotifyUsersAboutDrawer();
+  // };
 
-  var tempNotifyUsersAboutDrawer = function () {
-    if (window['localStorage'] && window['localStorage'].getItem('hasAcknowledgedDrawer') != "true") {
-      setTimeout(function() {
-        $(".drawer-controls a").addClass("shake");
-      }, 2500);
-    }
+  // var tempNotifyUsersAboutDrawer = function () {
+  //   if (window['localStorage'] && window['localStorage'].getItem('hasAcknowledgedDrawer') != "true") {
+  //     setTimeout(function() {
+  //       $(".drawer-controls a").addClass("shake");
+  //     }, 2500);
+  //   }
 
-    $(".drawer-controls a").on("click", function() {
-      window['localStorage'].setItem('hasAcknowledgedDrawer', true);
-    });
-  };
+  //   $(".drawer-controls a").on("click", function() {
+  //     window['localStorage'].setItem('hasAcknowledgedDrawer', true);
+  //   });
+  // };
 
-  var refreshDrawer = function () {
-    loadBoards();
-  };
+  // var refreshDrawer = function () {
+  //   loadBoards();
+  // };
 
-  var loadBoards = function() {
-    resetBoards("Loading boards, please wait...");
-    $.ajax({
-      url: "/boards",
-      method: "get",
-      headers: {
-        Accept: 'application/json'
-      },
-      dataType: "json",
-      success: loadBoardsCallback,
-      error: function(request, status, error) {
-        resetBoards(request.status === 400 ? 'No boards' : 'Error. Try reloading!');
-      }
-    });
+  // var loadBoards = function() {
+  //   resetBoards("Loading boards, please wait...");
+  //   $.ajax({
+  //     url: "/boards",
+  //     method: "get",
+  //     headers: {
+  //       Accept: 'application/json'
+  //     },
+  //     dataType: "json",
+  //     success: loadBoardsCallback,
+  //     error: function(request, status, error) {
+  //       resetBoards(request.status === 400 ? 'No boards' : 'Error. Try reloading!');
+  //     }
+  //   });
     
-  };
+  // };
 
-  var resetBoards = function(text) {
-    if (text) {
-      $("#config-drawer-board-list").append($("<span>" + text + "</span>"));
-    } else {
-      $("#config-drawer-board-list").empty();
-    }
-  };
-  var editProject = function(last_board_id,orgName,board_name){
-    window.location = "/new_board?org_id=nil&last_board_id="+last_board_id+"&orgName="+orgName+"&board_name="+board_name+"&edit=true";
-  };
-  var newProject = function(last_board_id,orgName){
-    window.location = "/new_board?org_id=nil&last_board_id="+last_board_id+"&orgName="+orgName+"&edit=false";
-  };
-  var newProjectOrg = function(org_id,orgName){
-    window.location = "/new_board?last_board_id=nil&org_id="+org_id+"&orgName="+orgName;
-  };
+  // var resetBoards = function(text) {
+  //   if (text) {
+  //     $("#config-drawer-board-list").append($("<span>" + text + "</span>"));
+  //   } else {
+  //     $("#config-drawer-board-list").empty();
+  //   }
+  // };
+  // var editProject = function(last_board_id,orgName,board_name){
+  //   window.location = "/new_board?org_id=nil&last_board_id="+last_board_id+"&orgName="+orgName+"&board_name="+board_name+"&edit=true";
+  // };
+  // var newProject = function(last_board_id,orgName){
+  //   window.location = "/new_board?org_id=nil&last_board_id="+last_board_id+"&orgName="+orgName+"&edit=false";
+  // };
+  // var newProjectOrg = function(org_id,orgName){
+  //   window.location = "/new_board?last_board_id=nil&org_id="+org_id+"&orgName="+orgName;
+  // };
 
-  var loadBoardsCallback = function(data) {
-    resetBoards();
-    var boardData = data['data'], boardItem, board, organization, boards;
+  // var loadBoardsCallback = function(data) {
+  //   resetBoards();
+  //   var boardData = data['data'], boardItem, board, organization, boards;
     
     
 
-    var count = 0;
-    var entroAlta = false;
-    var entroBaja = false;
-    var entroNo = false;
-    var entro=false;
+  //   var count = 0;
+  //   var entroAlta = false;
+  //   var entroBaja = false;
+  //   var entroNo = false;
+  //   var entro=false;
     
-      for (var orgName in boardData) {
-        if((orgName.includes("1. Urgentes") ) || (orgName.includes("2. Priorizados") ) || (orgName.includes("3. No Priorizados") )){
-          if(orgName.includes("1. Urgentes")){
-            entroAlta=true;
-          }
-          else if(orgName.includes("2. Priorizados")){
-            entroBaja=true;
-          }
-          else if(orgName.includes("3. No Priorizados")){
-            entroNo=true;
-          }
-          entro=true;
-          organization = boardData[orgName];
-          organizationBoards = $("<ul/>");
+  //     for (var orgName in boardData) {
+  //       if((orgName.includes("1. Urgentes") ) || (orgName.includes("2. Priorizados") ) || (orgName.includes("3. No Priorizados") )){
+  //         if(orgName.includes("1. Urgentes")){
+  //           entroAlta=true;
+  //         }
+  //         else if(orgName.includes("2. Priorizados")){
+  //           entroBaja=true;
+  //         }
+  //         else if(orgName.includes("3. No Priorizados")){
+  //           entroNo=true;
+  //         }
+  //         entro=true;
+  //         organization = boardData[orgName];
+  //         organizationBoards = $("<ul/>");
           
-          teams.push(orgName);
-          esAdmin=false;
-          for (var j = 0; j < organization.length; ++j) {
-            board = organization[j];
-            item = $("<li/>", {
-              role: "presentation1"
-            });
-            if(board.is_admin){
-              esAdmin=true;
-              item.append($("<a style='display: inline-block;margin-left: -30px !important;overflow-wrap: break-word;    max-width: 250px;' href=\"/boards/" + board.id + "\">" + board.name + "</a><a onclick=\"Ollert.editProject('"+board.id+"','"+orgName+"','"+board.name+"')\" style='display: inline-block;float: right;padding: 5px !important;'><span class='glyphicon glyphicon-pencil' aria-hidden='true' style=''></span></a>"));
-            }
-            else{
-              item.append($("<a style='display: inline-block;margin-left: -30px !important;overflow-wrap: break-word;    max-width: 250px;' href=\"/boards/" + board.id + "\">" + board.name + "</a>"));
-            }
-            organizationBoards.append(item);
-          }
-          var section = $("<li class='org' id=\""+orgName+"\" role=\"presentation2\"><b>" + orgName + "</b></li>").append(organizationBoards)
-          var section2;
-          if(esAdmin){
-            var botonAgregar = $("<button id='boton_agregar' onclick=\"Ollert.newProject('"+board.id+"','"+orgName+"')\">Nuevo Proyecto</button>");
-            section2 = section.append(botonAgregar);
-          }
-          else{
-            section2=section
-          }
-          $("#config-drawer-board-list").append(section2);
-        }
-        else{
-          organization = boardData[orgName];
-          board=organization[0];
-          if(board.is_admin){
-            esAdmin=true;
-          }
-        }
+  //         teams.push(orgName);
+  //         esAdmin=false;
+  //         for (var j = 0; j < organization.length; ++j) {
+  //           board = organization[j];
+  //           item = $("<li/>", {
+  //             role: "presentation1"
+  //           });
+  //           if(board.is_admin){
+  //             esAdmin=true;
+  //             item.append($("<a style='display: inline-block;margin-left: -30px !important;overflow-wrap: break-word;    max-width: 250px;' href=\"/boards/" + board.id + "\">" + board.name + "</a><a onclick=\"Ollert.editProject('"+board.id+"','"+orgName+"','"+board.name+"')\" style='display: inline-block;float: right;padding: 5px !important;'><span class='glyphicon glyphicon-pencil' aria-hidden='true' style=''></span></a>"));
+  //           }
+  //           else{
+  //             item.append($("<a style='display: inline-block;margin-left: -30px !important;overflow-wrap: break-word;    max-width: 250px;' href=\"/boards/" + board.id + "\">" + board.name + "</a>"));
+  //           }
+  //           organizationBoards.append(item);
+  //         }
+  //         var section = $("<li class='org' id=\""+orgName+"\" role=\"presentation2\"><b>" + orgName + "</b></li>").append(organizationBoards)
+  //         var section2;
+  //         if(esAdmin){
+  //           var botonAgregar = $("<button id='boton_agregar' onclick=\"Ollert.newProject('"+board.id+"','"+orgName+"')\">Nuevo Proyecto</button>");
+  //           section2 = section.append(botonAgregar);
+  //         }
+  //         else{
+  //           section2=section
+  //         }
+  //         $("#config-drawer-board-list").append(section2);
+  //       }
+  //       else{
+  //         organization = boardData[orgName];
+  //         board=organization[0];
+  //         if(board.is_admin){
+  //           esAdmin=true;
+  //         }
+  //       }
         
 
-      }
-      if(entro==true){
-        count=count+1;
-        entro=false;
-      }
+  //     }
+  //     if(entro==true){
+  //       count=count+1;
+  //       entro=false;
+  //     }
     
-    teams.sort(function(a, b){
-      if(a.toString() < b.toString()) return -1;
-      if(a.toString() > b.toString()) return 1;
-      return 0;
-    });
-    $.ajax({
-      url: "/organizations",
-      method: "get",
-      headers: {
-        Accept: 'application/json'
-      },
-      dataType: "json",
-      success: loadOrganizationsCallback,
-      error: function(request, status, error) {
-        resetBoards(request.status === 400 ? 'No organizations' : 'Error. Try reloading!');
-      }
-    });
-  };
+  //   teams.sort(function(a, b){
+  //     if(a.toString() < b.toString()) return -1;
+  //     if(a.toString() > b.toString()) return 1;
+  //     return 0;
+  //   });
+  //   $.ajax({
+  //     url: "/organizations",
+  //     method: "get",
+  //     headers: {
+  //       Accept: 'application/json'
+  //     },
+  //     dataType: "json",
+  //     success: loadOrganizationsCallback,
+  //     error: function(request, status, error) {
+  //       resetBoards(request.status === 400 ? 'No organizations' : 'Error. Try reloading!');
+  //     }
+  //   });
+  // };
 
-  var loadOrganizationsCallback = function(data){
-    var appender="";
-    var orgData = data['data'];
-    var orgName;
-    for (i = 0; i < orgData.length; i++) {
-      orgName = orgData[i]['displayName'];
-      var esta=false;
-      for(t = 0; t< teams.length; t++){
-        if(teams[t]==orgName){
-          esta=true;
-          break;
-        }
-      }
-      if(esta==false && esAdmin){
-        if(orgName.includes("1. Urgentes") || orgName.includes("2. Priorizados") || orgName.includes("3. No Priorizados")){
-          if(orgName.includes("1. Urgentes")){
-            appender=appender+"<li class='org' id='1. Urgentes' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
+  // var loadOrganizationsCallback = function(data){
+  //   var appender="";
+  //   var orgData = data['data'];
+  //   var orgName;
+  //   for (i = 0; i < orgData.length; i++) {
+  //     orgName = orgData[i]['displayName'];
+  //     var esta=false;
+  //     for(t = 0; t< teams.length; t++){
+  //       if(teams[t]==orgName){
+  //         esta=true;
+  //         break;
+  //       }
+  //     }
+  //     if(esta==false && esAdmin){
+  //       if(orgName.includes("1. Urgentes") || orgName.includes("2. Priorizados") || orgName.includes("3. No Priorizados")){
+  //         if(orgName.includes("1. Urgentes")){
+  //           appender=appender+"<li class='org' id='1. Urgentes' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
 
-          }
-          else if(orgName.includes("2. Priorizados")){
-            appender=appender+"<li class='org' id='2. Priorizados' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
+  //         }
+  //         else if(orgName.includes("2. Priorizados")){
+  //           appender=appender+"<li class='org' id='2. Priorizados' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
 
-          }
-          else if(orgName.includes("3. No Priorizados")){
-            appender=appender+"<li class='org' id='3. No Priorizados' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
+  //         }
+  //         else if(orgName.includes("3. No Priorizados")){
+  //           appender=appender+"<li class='org' id='3. No Priorizados' role=\"presentation\"><b>" + orgName + "</b><ul style='padding-left: 0px;'></ul><button onclick=\"Ollert.newProjectOrg('"+orgData[i]['id']+"','"+orgName+"')\">Nuevo Proyecto</button></li>"
 
-          }
+  //         }
         
-        }
-      }  
-    }
-    $("#config-drawer-board-list").append(appender);
-    var nodos=$(".org");
-    for(var t=0;t<nodos.length;t++){
-      for(var i=0;i<nodos.length;i++){
-        if(nodos[i].id.includes("2. Priorizados")){
-          $("#config-drawer-board-list").append(nodos[i]);
-          nodos=$(".org");
-        }
-        if(nodos[i].id.includes("3. No Priorizados")){
-          $("#config-drawer-board-list").append(nodos[i]);
-          nodos=$(".org");
-        }
-      }
-    }
-  };
+  //       }
+  //     }  
+  //   }
+  //   $("#config-drawer-board-list").append(appender);
+  //   var nodos=$(".org");
+  //   for(var t=0;t<nodos.length;t++){
+  //     for(var i=0;i<nodos.length;i++){
+  //       if(nodos[i].id.includes("2. Priorizados")){
+  //         $("#config-drawer-board-list").append(nodos[i]);
+  //         nodos=$(".org");
+  //       }
+  //       if(nodos[i].id.includes("3. No Priorizados")){
+  //         $("#config-drawer-board-list").append(nodos[i]);
+  //         nodos=$(".org");
+  //       }
+  //     }
+  //   }
+  // };
 
   
 
@@ -213,13 +213,13 @@ var Ollert = (function() {
   };
 
   return {
-    initDrawer: initDrawer,
-    refreshDrawer: refreshDrawer,
+    //initDrawer: initDrawer,
+    //refreshDrawer: refreshDrawer,
     loadAvatar: loadAvatar,
-    newProject: newProject,
-    newProjectOrg: newProjectOrg,
-    editProject: editProject,
-    teams: teams,
+    //newProject: newProject,
+    //newProjectOrg: newProjectOrg,
+    //editProject: editProject,
+    //teams: teams,
     info: info,
     esAdmin: esAdmin
   };
