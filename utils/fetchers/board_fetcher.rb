1bd4efba655d8410c7abb8fce94203c8270f58b5
nicolas schmidt <nschmidtg@gmail.com>
2016-10-11 15:33:03 -0300

    arreglo de proyectos que se veían en la gestión siendo que estaban cerrados

--- utils/fetchers/board_fetcher.rb
+++ utils/fetchers/board_fetcher.rb
@@ -3,7 +3,7 @@ require 'trello'
 class BoardFetcher
   def self.fetch(client, member_id)
     raise Trello::Error if client.nil? || member_id.nil? || member_id.empty?
-    JSON.parse(client.get("/members/#{member_id}/boards", {fields: :name, organization: true}))
+    JSON.parse(client.get("/members/#{member_id}/boards", {fields: "name,closed", organization: true}))
   end
 
   def self.fetch2(client, member_id)
