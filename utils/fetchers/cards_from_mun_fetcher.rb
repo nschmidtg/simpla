6023c2d6c8f1dad33a2d00eaef91c6939b075477
nicolas schmidt <nschmidtg@gmail.com>
2016-12-13 17:57:36 -0300

    2archivado en el archivo

--- utils/fetchers/cards_from_mun_fetcher.rb
+++ utils/fetchers/cards_from_mun_fetcher.rb
@@ -6,9 +6,12 @@ class CardsFromMunFetcher
     cards=Array.new
     Municipio.find_by(id: mun_id).boards.where(:current_state.nin => ["Descartado"]).each do |board|
     	board_id=board.board_id
+        boardName=""
     	a=JSON.parse(client.get("/boards/#{board_id}", options))
     	a["cards"].each do |card|
-        card["boardName"]=a["name"]
+            boardName=card["boardName"]
+            card["boardName"]=a["name"]
+            card["finish"]=board.end_date
     		cards<<card
     	end
     end
