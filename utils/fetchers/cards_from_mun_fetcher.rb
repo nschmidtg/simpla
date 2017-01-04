991878a2a757dc165bd6b793e62d9b07cf165f41
nicolas schmidt <nschmidtg@gmail.com>
2017-01-04 16:32:13 -0300

    Agregar fecha de fin del proyecto al calendario general

--- utils/fetchers/cards_from_mun_fetcher.rb
+++ utils/fetchers/cards_from_mun_fetcher.rb
@@ -6,14 +6,21 @@ class CardsFromMunFetcher
     cards=Array.new
     Municipio.find_by(id: mun_id).boards.where(:current_state.nin => ["Descartado"]).each do |board|
     	board_id=board.board_id
-        boardName=""
+      boardName=""
     	a=JSON.parse(client.get("/boards/#{board_id}", options))
     	a["cards"].each do |card|
-            boardName=card["boardName"]
-            card["boardName"]=a["name"]
-            #card["finish"]=board.end_date
-    		cards<<card
+      boardName=card["boardName"]
+      card["boardName"]=a["name"]
+      cards<<card
     	end
+      if(board.end_date!=nil && board.end_date!="")
+        card=Hash.new
+        card["boardName"]=a["name"]
+        card["due"]=DateTime.parse(board.end_date).to_s.split('T')[0]+"T15:00:00.000Z"
+        card["closed"]=false
+        card["name"]="Fecha estimada de fin del proyecto"
+        cards<<card
+      end
     end
     return cards
   end
