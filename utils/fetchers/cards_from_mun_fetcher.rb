390a32f2e75e1ccf82dcfd75cff73b932e496f25
nicolas schmidt <nschmidtg@gmail.com>
2017-01-04 17:17:01 -0300

    archivos adjuntos

--- utils/fetchers/cards_from_mun_fetcher.rb
+++ utils/fetchers/cards_from_mun_fetcher.rb
@@ -4,7 +4,7 @@ class CardsFromMunFetcher
   def self.fetch(client, mun_id)
     options = {filter: :open, lists: :open, cards: :open, fields: "name,"}
     cards=Array.new
-    Municipio.find_by(id: mun_id).boards.where(:current_state.nin => ["Descartado"]).each do |board|
+    Municipio.find_by(id: mun_id).boards.where(:current_state.nin => ["Descartado","Finalizado"]).each do |board|
     	board_id=board.board_id
       boardName=""
     	a=JSON.parse(client.get("/boards/#{board_id}", options))
